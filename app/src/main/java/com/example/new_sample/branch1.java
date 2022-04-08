a3863ea2f776b5eb2ec3c568d287281f77e85ab0
Exoalam <nafiul.arpon@gmail.com>
2022-04-08 23:16:29 +0600

    adding class to branch

---
+++ app/src/main/java/com/example/new_sample/branch1.java
@@ -0,0 +1,26 @@
+package com.example.new_sample;
+
+public class branch1 {
+    public int id;
+    public String name;
+
+    public branch1() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
