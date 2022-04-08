2d035d8ca45d3bdca7fa59ec616e743d002dd52f
Exoalam <nafiul.arpon@gmail.com>
2022-04-08 23:06:55 +0600

    Master demo added

---
+++ app/src/main/java/com/example/new_sample/master_demo.java
@@ -0,0 +1,26 @@
+package com.example.new_sample;
+
+public class master_demo {
+    public int id;
+    public String name;
+
+    public master_demo() {
+        System.out.println("master_demo");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
